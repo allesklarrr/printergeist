82517ff1607884f5dee74f4430a6bf445dbbd5aa
mvghasty <fasmagoric.dev@gmail.com>
2025-07-07 01:34:46 -0300

    Code refactor, New TUI with prompt toolkit

--- src/main.py
+++ src/main.py
@@ -1,30 +1,11 @@
-import cups
-import json
+import sys
 import os
-import mimetypes
+import cups
 import time
-import shutil
-from rich import box
-from rich.console import Console
-from rich.table import Table
-from rich.progress import Progress, SpinnerColumn, TextColumn
-
-os.system("clear")
-
-largura = shutil.get_terminal_size().columns
-reset = "\033[0m"
-textc = "\033[38;5;16m"
-vermelho = "\033[48;5;196m" 
-amarelo  = "\033[48;5;226m"
-verde    = "\033[48;5;46m" 
-
-console = Console()
-
-console.print("="*largura)
-print(("ᕕ( ᐛ )ᕗ  ~  P R I N T E R G E I S T  ~  ᕕ( ᐛ )ᕗ").center(largura))
-print(("Copyright (c) 2025 mvghasty - https://github.com/mvghasty/printergeist").center(largura))
-print(("LICENSE: GNU General Public License, 2.0").center(largura))
-print("="*largura)
+import json
+import mimetypes
+from prompt_toolkit.shortcuts.dialogs import input_dialog, button_dialog, message_dialog
+from prompt_toolkit.styles import Style
 
 def format_bytes(size):
     for unit in ['B', 'KB', 'MB', 'GB']:
@@ -33,81 +14,129 @@ def format_bytes(size):
         size /= 1024.0
     return f"{size:.1f} TB"
 
-conn = cups.Connection()
-printers = conn.getPrinters()
+def get_printer():
+    conn = cups.Connection()
+    printers = conn.getPrinters()
 
-printer_name = None
-for name in printers:
-    if "L3250" in name.upper() or "EPSON" in name.upper():
-        printer_name = name
-        break
+    for name, info in printers.items():
+        device_uri = info.get("device-uri", "")
+        if device_uri.startswith("usb://"):
+            return conn, name, printers
 
-if not printer_name:
-    print(f"{vermelho}{textc} [	ಠ_ಠ ] - ERROR: Impressora Epson L3250 não encontrada.{reset}")
-    exit(1)
+    if printers:
+        first_name = next(iter(printers))
+        return conn, first_name, printers
 
-file_path = input("[  π  ] - Digite o nome ou caminho do arquivo que deseja imprimir: ").strip()
+    return None, None, printers
 
-if not os.path.isfile(file_path):
-    error_table_input = Table(title="\n[ ಠ_ಠ ] ! ERROR ! [ ಠ_ಠ ]", box=None, title_style="bold black on red")
-    error_table_input.add_column("INPUT_ERROR", justify="center", header_style="bold black on red", style="bold black on red")
-    error_table_input.add_row(
-        f"Arquivo '{file_path}' não encontrado.\n"
-    )
-    console.print(error_table_input)
-    exit(1)
+style = Style.from_dict({
+    'dialog':             'bg:#007AB9',
+    'dialog frame.label': 'bg:#ffffff #000000',
+    'dialog.body':        'bg:#ffffff #000000',
+    'dialog shadow':      'bg:#333333',
+})
 
-os.system("clear")
-mime_type, _ = mimetypes.guess_type(file_path)
-file_size = os.path.getsize(file_path)
-file_name = os.path.basename(file_path)
+def FILE_INPUT():
+    path = input_dialog(
+        title="ᕕ( ᐛ )ᕗ  ~  P R I N T E R G E I S T  ~  ᕕ( ᐛ )ᕗ",
+        text="* FILE PATH FOR PRINTING",
+        style=style
+    ).run()
+    if path == None:
+        sys.exit(0)
+    return path
 
-is_color = not ("gray" in printer_name.lower() or "bw" in printer_name.lower())
+def MENU():
+    application_menu = button_dialog(
+        title="ᕕ( ᐛ )ᕗ  ~  P R I N T E R G E I S T  ~  ᕕ( ᐛ )ᕗ",
+        text="""
+            ----------------------------------------------------------------------
+            Copyright (c) 2025 mvghasty - https://github.com/mvghasty/printergeist
+            LICENSE: GNU General Public License, 2.0
+            ----------------------------------------------------------------------
+            """,
+        buttons=[
+            ('INIT', 1),
+            ('EXIT', 2),
+        ],
+        style=style
+    ).run()
 
-table = Table(title=" (⌐⊙_⊙) Detalhes da Impressão", box=box.HORIZONTALS, border_style="black on yellow", title_style="bold black on yellow")
-table.add_column("Arquivo", style="dark_green on yellow", header_style="bold black on yellow")
-table.add_column("Tipo", justify="center", style="dark_green on yellow", header_style="bold black on yellow")
-table.add_column("Tamanho", justify="right", style="dark_green on yellow", header_style="bold black on yellow")
-table.add_column("Colorido", justify="center", style="dark_green on yellow", header_style="bold black on yellow")
+    match application_menu:
+        case 1:
+            return FILE_INPUT()
+        case 2:
+            sys.exit(0)
 
-table.add_row(
-    file_name,
-    mime_type or "desconhecido",
-    format_bytes(file_size),
-    "Sim" if is_color else "Não"
-)
+def main():
+    file_path = MENU()
 
-console.print("="*largura)
-print(("CUPS REQUEST").center(largura))
-print("="*largura)
-console.print(table)
+    if not file_path or not os.path.isfile(file_path):
+        message_dialog(
+            title="Error",
+            text="Invalid file path provided.",
+            style=style
+        ).run()
+        sys.exit(1)
 
-with Progress(
-    SpinnerColumn(),
-    TextColumn("[progress.description]{task.description}"),
-    transient=True,
-) as progress:
-    progress.add_task(description=f"Enviando request para a impressora {printer_name}...", total=None)
+    conn, printer_name, printers_info = get_printer()
+    if not printer_name:
+        message_dialog(
+            title="Error",
+            text="No printer found.",
+            style=style
+        ).run()
+        sys.exit(1)
+
+    mime_type, _ = mimetypes.guess_type(file_path)
+    file_size = os.path.getsize(file_path)
+    file_name = os.path.basename(file_path)
+    is_color = not ("gray" in printer_name.lower() or "bw" in printer_name.lower())
+
+    message_dialog(
+        title="PRINT REQUEST", 
+        text= f""" 
+            Archive: {file_name}
+            MIME type: {mime_type}
+            Size: {format_bytes(file_size)}
+            Colorful: {"Yes" if is_color else "No"}
+            ----------------------------------------------------------------------
+            """,
+        style=style
+    ).run()
+
+    print(f"[ * ] Sending request: {printer_name}...")
+
     try:
         job_id = conn.printFile(
             printer_name,
             file_path,
-            "REQUEST VIA PRINTERGEIST",
+            f"PRINTERGEIST REQUEST - {file_name}",
             {"document-format": mime_type or "application/octet-stream"}
         )
         time.sleep(2)
     except cups.IPPError as e:
-        error_table_press = Table(title="\n[ ಠ_ಠ ] ! ERROR ! [ ಠ_ಠ ]", box=None, title_style="bold black on red")
-        error_table_press.add_column("REQUEST_ERROR", justify="center", header_style="bold black on red", style="bold black on red")
-        error_table_press.add_row(
-            f"Erro ao enviar impressão: {e}\n"
-        )
-        console.print(error_table_press)
-        exit(1)
-console.print(f"\n[bold bright_white on green][ (^‿^) ] - Impressão enviada com sucesso! Job ID: {job_id} [/bold bright_white on green]\n")
-console.print("="*largura)
-print(("CUPS DEBUG-LOG").center(largura))
-print(("(JSON FILE FORMAT)").center(largura))
-print("="*largura)
-console.print(json.dumps(printers, indent=4, separators=(", ", " = ")), style="dim")
-print("="*largura)
+        message_dialog(
+            title="ERROR",
+            text=f"Request error for: {e}",
+            style=style
+        ).run()
+        sys.exit(1)
+
+    message_dialog(
+        title="PRINT REQUEST",
+        text=f"""
+            [ ✓ ] Printing made successfully! Job ID: {job_id}")
+            ----------------------------------------------------------------------
+            CUPS DEBUG-LOG
+            (JSON FORMAT)
+            ----------------------------------------------------------------------
+            {json.dumps(printers_info, indent=4, separators=(", ", " = "))}
+            """,
+        style=style
+    ).run()
+    
+    print(json.dumps(printers_info, indent=4, separators=(", ", " = ")))
+
+if __name__ == "__main__":
+    main()
